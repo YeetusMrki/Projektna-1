17fc06eb8114b04637a2b4fb578ee14f06b636b6
YeetusMrki <amelmrkaljevic06@gmail.com>
2025-11-21 10:23:24 +0100

    Add files via upload

---
+++ javaScript/form.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+	const form = document.querySelector("form");
+	
+	form.addEventListener("submit", function (e) {
+		e.preventDefault();
+		if(!form.checkValidity()) {
+			form.reportValidity();
+			Swal.fire({
+				icon: "Error",
+				title: "Oops",
+				text: "One or more of your fields are empty!"
+			});
+			return;
+		}
+		
+		Swal.fire({
+		  title: "You have successfully created an acount!",
+		  showClass: {
+			popup: `
+			  animate__animated
+			  animate__fadeInUp
+			  animate__faster
+			`
+		  },
+		  hideClass: {
+			popup: `
+			  animate__animated
+			  animate__fadeOutDown
+			  animate__faster
+			`
+		  }
+		});
+		
+		setTimeout(()=>form.reset(),1500);
+		
+	});
+});
